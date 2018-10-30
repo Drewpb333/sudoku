3d3eb51c48d10277e0c2ac0b8e91ed692ff0219a
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-10-30 13:09:39 -0400

    Updates

---
+++ precache-manifest.59ee81bda09aa667df93699dd35ef038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "738a16a178d39df74782",
+    "url": "/sudoku/static/js/runtime~main.738a16a1.js"
+  },
+  {
+    "revision": "a58aaa631dcf95ba2a3c",
+    "url": "/sudoku/static/js/main.a58aaa63.chunk.js"
+  },
+  {
+    "revision": "010cccb7859b15208b56",
+    "url": "/sudoku/static/js/1.010cccb7.chunk.js"
+  },
+  {
+    "revision": "a58aaa631dcf95ba2a3c",
+    "url": "/sudoku/static/css/main.0915fc96.chunk.css"
+  },
+  {
+    "revision": "a09bc199bf21cb5885355db99032e228",
+    "url": "/sudoku/index.html"
+  }
+];
